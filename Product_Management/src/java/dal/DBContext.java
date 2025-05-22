69f06d3e81233ed0ed4a35a416226b206e2313a0
Pham Duc Trong <TrongPDHE181640@fpt.edu.vn>
2025-05-22 21:50:01 +0700

    Update DBContext

---
+++ Product_Management/src/java/dal/DBContext.java
@@ -0,0 +1,48 @@
+package DAO;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBContext {
+    private static DBContext instance = new DBContext();
+    private Connection connection;
+
+    //Static lanf 2
+    public static DBContext getInstance() {
+        return instance;
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+    private DBContext() {
+        try {
+            if (connection == null || connection.isClosed()) {
+                String user = "root";
+                String password = "msqldt154A!";
+                String url = "jdbc:mysql://localhost:3306/project_g2?useSSL=false&serverTimezone=UTC";
+
+                Class.forName("com.mysql.cj.jdbc.Driver");
+                connection = DriverManager.getConnection(url, user, password);
+                System.out.println("✅ Kết nối MySQL thành công!");
+            }
+        } catch (Exception e) {
+            connection = null;
+            System.out.println("❌ Kết nối MySQL thất bại: " + e.getMessage());
+        }
+    }
+
+    public static void testConnection() {
+        Connection conn = DBContext.getInstance().getConnection();
+        if (conn != null) {
+            System.out.println("✅ Database đã kết nối!");
+        } else {
+            System.out.println("❌ Không thể kết nối đến database!");
+        }
+    }
+
+    public static void main(String[] args) {
+        testConnection();
+    }
+}
